37c960dfaf584e65ec0a85a7ba3b0430231672fb
Kshama Raichura <kshamaraichura@gmail.com>
2020-09-27 11:46:46 +1000

    Added model, controller and route for Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // Get all products
+    public function getAll() {
+    }
+
+    // Get all products
+    public function get($id) {
+    }
+
+    // Add a new product
+    public function add(Request $request) {
+
+    }
+
+    // Edit a new product
+    public function edit(Request $request, $id) {
+
+    }
+
+    // Delete a new product
+    public function delete($id) {
+
+    }
+
+    // Filter products by price, bedrooms and bathrooms
+    public function filter(Request $request) {
+
+    }
+}
---
+++ database/migrations/2020_09_27_013118_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 20, 2);
+            $table->text('description');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->boolean('publish')->default(1);
+            $table->string('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
